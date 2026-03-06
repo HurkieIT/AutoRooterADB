4547e23f58e1d9b5973f73060a72bc6e380c40f8
HurkieIT <jensvandenhurk@outlook.com>
2026-03-06 11:36:23 +0100

    readme

--- autorooter.sh
+++ autorooter.sh
@@ -4,22 +4,6 @@ set -euo pipefail
 #AutoRooter voor Android devices (BASH)
 #Gemaakt door: Jens van den Hurk
 
-#DISCLAIMER: Dit werkt vanaf Android 5 tot Android 13.
-#Dit script runt een half automatische rooter van generieke android devices, dit kunnen devices zijn zoals OnePlus, Xiaomi, Samsung en soort gelijk.
-#Dit blijft altijd nog een White Glove provisioning script, waarbij altijd user interaction nog vereist wordt tijdens de doorloop van dit script
-
-#BENODIGDHEDEN:
-# - Android device met minimaal Android 5.0
-# - USB kabel
-# - Computer met ADB en Fastboot geïnstalleerd
-# - OEM Unlocking enabled in de developer options van het Android device
-# - USB Debugging enabled in de developer options van het Android device
-# - Magisk (of een vergelijkbare tool) voor het patchen van de boot image
-# - Minimaal 50% batterij niveau op het Android device
-# - Back-up van belangrijke data op het Android device (aanbevolen)
-
-#_________________________________________________________
-
 #Kleuren voor de variabelen
 #Alle kleuren bieden zijn eigen informatie; Rood is critical/error, Yellow is warning, Green is Success en Blue is Information.
 #dit biedt overzicht tijdens het proces zodat de user ook duidelijk kan zien wat er allemaal gebeurd.
---
+++ readme.md
@@ -0,0 +1,15 @@
+DISCLAIMER: Dit werkt vanaf Android 5 tot Android 13.
+Dit script runt een half automatische rooter van generieke android devices, dit kunnen devices zijn zoals OnePlus, Xiaomi, Samsung en soort gelijk.
+Dit blijft altijd nog een White Glove provisioning script, waarbij altijd user interaction nog vereist wordt tijdens de doorloop van dit script
+
+BENODIGDHEDEN:
+# - Android device met minimaal Android 5.0
+# - USB kabel
+# - Computer met ADB en Fastboot geïnstalleerd
+# - OEM Unlocking enabled in de developer options van het Android device
+# - USB Debugging enabled in de developer options van het Android device
+# - Magisk (of een vergelijkbare tool) voor het patchen van de boot image
+# - Minimaal 50% batterij niveau op het Android device
+# - Back-up van belangrijke data op het Android device (aanbevolen)
+
+De Documentatie bevindt zich in de opdracht zelf, de bronnen en eventuele inspiraties zijn benoemd in de code zelf.
